1d7e91f4fbbc8324ae8630490cc181e5361a5db8
Eduardo Souza <edcampossouza@gmail.com>
2023-02-08 09:29:02 -0300

    init express app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
