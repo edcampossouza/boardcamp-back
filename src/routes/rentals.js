eba3cfda5d0f9eee05ed701e2708fd1f603b5cab
Eduardo Souza <edcampossouza@gmail.com>
2023-02-08 18:58:01 -0300

    fix: endpoint name for rental return

--- src/routes/rentals.js
+++ src/routes/rentals.js
@@ -12,7 +12,7 @@ const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
 rentalsRouter.post("/rentals", validateSchema(rentalInput), postRental);
-rentalsRouter.post("/rentals/:id/finish", finishRental);
+rentalsRouter.post("/rentals/:id/return", finishRental);
 rentalsRouter.delete("/rentals/:id", deleteRental);
 
 export default rentalsRouter;
