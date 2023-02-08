d5b2cba2fd426ae6b671292826cf9fffb247f212
Eduardo Souza <edcampossouza@gmail.com>
2023-02-08 15:42:19 -0300

    hub: set ssl to false
    hub fell into infinite loop
    
    random attempt at fixing it

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,8 @@ dotenv.config();
 const { Pool } = pg;
 
 const { DATABASE_URL } = process.env;
+const MODE = process.env.MODE || "dev";
 export const db = new Pool({
   connectionString: DATABASE_URL,
+  ssl: MODE === "prod",
 });
