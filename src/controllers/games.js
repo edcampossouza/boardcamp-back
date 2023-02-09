c7cc40eec009460740563e64d4e0236ab7aafc44
Eduardo Souza <edcampossouza@gmail.com>
2023-02-08 21:49:55 -0300

    feat: GET /games pagination

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -2,10 +2,21 @@ import { db } from "../config/database.js";
 
 export async function getGames(req, res) {
   const { name } = req.query;
-  const query = name
+  const { offset, limit } = req.query;
+  let queryIndex = 1;
+  let query = name
     ? "SELECT * FROM games where LOWER(name) like LOWER($1)"
     : "SELECT * FROM games";
   const values = name ? [`${name}%`] : [];
+  if (name) queryIndex++;
+  if (offset) {
+    query += ` OFFSET $${queryIndex++} `;
+    values.push(offset);
+  }
+  if (limit) {
+    query += ` LIMIT $${queryIndex++} `;
+    values.push(limit);
+  }
   try {
     const { rows } = await db.query(query, values);
     return res.status(200).send(rows);
